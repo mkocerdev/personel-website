c2492f64b42e3869701decd3629fed22dc639afd
mehmetkocer <mehmetkocer2@hotmail.com>
2020-11-02 22:55:56 +0300

    Footer Spotify icon added

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,4 @@
 export { default as Github } from "./github";
 export { default as Linkedin } from "./linkedin";
 export { default as Twitter } from "./twitter";
+export { default as Spotify } from "./spotify";
--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -2,16 +2,45 @@ import React from "react";
 
 import styles from "./footer.module.css";
 
-import { Github, Linkedin, Twitter } from "../icons";
+import { Github, Linkedin, Twitter, Spotify } from "../icons";
 
 const Footer = () => (
   <footer className={styles.footer}>
     <div className="container">
       <div className={styles.wrapper}>
         <div className={styles.socialmedia}>
-          <Twitter className={styles.icon} />
-          <Github className={styles.icon} />
-          <Linkedin className={styles.icon} />
+          <a
+            href="https://twitter.com/mkocerdev/"
+            target="__blank"
+            rel="noopener noreferrer"
+            title="Twitter"
+          >
+            <Twitter className={styles.icon} />
+          </a>
+          <a
+            href="https://github.com/mkocerdev/"
+            target="__blank"
+            rel="noopener noreferrer"
+            title="Github"
+          >
+            <Github className={styles.icon} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/mkocerdev/"
+            target="__blank"
+            rel="noopener noreferrer"
+            title="LinkedIn"
+          >
+            <Linkedin className={styles.icon} />
+          </a>
+          <a
+            href="https://open.spotify.com/user/zimsterm/"
+            target="__blank"
+            rel="noopener noreferrer"
+            title="Spotify"
+          >
+            <Spotify className={styles.icon} />
+          </a>
         </div>
         <div className={styles.made}>
           © {new Date().getFullYear()},
