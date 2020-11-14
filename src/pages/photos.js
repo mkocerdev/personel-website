1a549b624720b85ae3fea786b3bae5d7ace95ae1
mehmetkocerr <46313560+mehmetkocerr@users.noreply.github.com>
2020-11-14 19:21:45 +0300

    Photos list responsive and query filter sort added

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,43 +1,44 @@
-import React from "react";
-import { graphql } from "gatsby";
+import React from 'react';
+import { graphql } from 'gatsby';
 
-import { Layout, SEO, PhotoList, PhotoBox } from "../components";
+import { Layout, SEO, PhotoList, PhotoBox } from '../components';
 
 const PhotosPage = ({ data: { Photos } }) => (
-  <Layout>
-    <SEO title="Photos" />
-    <div className="container">
-      <h1>Photos</h1>
-      <div className="photos">
-        <PhotoList column="2">
-          {Photos.edges.map(({ node }) => {
-            return <PhotoBox key={node.id} {...node}></PhotoBox>;
-          })}
-        </PhotoList>
-      </div>
-    </div>
-  </Layout>
+	<Layout>
+		<SEO title='Photos' />
+		<div className='container'>
+			<h1>Photos</h1>
+			<div className='photos'>
+				<PhotoList column='2'>
+					{Photos.edges.map(({ node }) => {
+						return <PhotoBox key={node.id} {...node}></PhotoBox>;
+					})}
+				</PhotoList>
+			</div>
+		</div>
+	</Layout>
 );
 
 export const pageQuery = graphql`
-  {
-    Photos: allFile(
-      filter: { absolutePath: { regex: "//photos//" } }
-      limit: 30
-    ) {
-      edges {
-        node {
-          id
-          name
-          birthTime(formatString: "MMMM DD YYYY MM:SS", locale: "en")
-          img: childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
+	{
+		Photos: allFile(
+			filter: { absolutePath: { regex: "//photos//" } }
+			limit: 30
+			sort: { fields: birthTime, order: DESC }
+		) {
+			edges {
+				node {
+					id
+					name
+					birthTime(formatString: "MMMM DD YYYY MM:SS", locale: "en")
+					img: childImageSharp {
+						fluid {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+			}
+		}
+	}
 `;
 export default PhotosPage;
